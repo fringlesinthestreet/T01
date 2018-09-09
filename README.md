6429a394e4f60563c67c025b0e319cc170f87c32
fringlesinthestreet <bicarcamo@uc.cl>
2018-09-09 08:04:48 -0300

    app ready

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 **Autor:** [Benjamín Cárcamo](github.com/fringlesinthestreet)
 
 Me basé en la aplicación hecha en este [link](https://realpython.com/flask-by-example-part-3-text-processing-with-requests-beautifulsoup-nltk/).
+
+El manejo de errores lo saqué de este [link](https://guillaumegenthial.github.io/serving.html).
--- app.py
+++ app.py
@@ -1,17 +1,57 @@
 import os
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, request
+from models import Posts, app, db
+import datetime
+import time
 
-
-app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    errors = []
+    if request.method == "POST":
+        try:
+            text = request.form['text']
+            ip = request.remote_addr
+            ts = time.time()
+            date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+            print(text, ip, date)
+            #posts.append(text)
+            post = Posts(text, ip, date)
+            db.session.add(post)
+            db.session.commit()
+        except KeyError:
+            errors.append(
+                "Bad Request (400)"
+            )
+        except Exception as err:
+            errors.append(
+                "Ocurrió un error: {}".format(err)
+            )
 
+    posts = Posts.query.order_by(Posts.id).all()
+
+    return render_template('index.html', errors=errors, posts=posts)
+
+
+@app.errorhandler(404)
+def url_error(e):
+    return """
+    Wrong URL!
+    <pre>{}</pre>""".format(e), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+@app.errorhandler(400)
+def bad_request(e):
+    return """
+    Bad Request attempted <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
 
 if __name__ == '__main__':
     app.run()
