9f08e65cba95a7b5dc0bac1fc491cce7d280734a
fringlesinthestreet <bicarcamo@uc.cl>
2018-09-09 07:08:10 -0300

    database configuration ready

---
+++ models.py
@@ -0,0 +1,19 @@
+from app import db
+
+
+class Posts(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(500), nullable=False)
+    ip = db.Column(db.String(500), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+
+
+    def __init__(self, text, ip, date):
+        self.text = text
+        self.ip = ip
+        self.date = date
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
