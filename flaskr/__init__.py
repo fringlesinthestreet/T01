44da4829c64fe3cd4ca71545c445ff719adb2a73
fringlesinthestreet <bicarcamo@uc.cl>
2018-09-11 09:56:50 -0300

    changing path to models module

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,7 +5,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(os.environ['APP_SETTINGS'])
 
-    from models import db
+    from flaskr.models import db
     db.init_app(app)
 
     return app
