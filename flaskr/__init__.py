8f7a2bdf312e2a2cd300b320a80ab687d2e71875
fringlesinthestreet <bicarcamo@uc.cl>
2018-09-11 09:58:51 -0300

    moving commands

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 import os
+from flask_sqlalchemy import SQLAlchemy
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(os.environ['APP_SETTINGS'])
 
-    from flaskr.models import db
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db = SQLAlchemy(app)
+
     db.init_app(app)
 
     return app
--- flaskr/models.py
+++ flaskr/models.py
@@ -3,9 +3,6 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
 class Posts(db.Model):
     __tablename__ = 'posts'
 
