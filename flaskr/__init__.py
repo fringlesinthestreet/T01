3fb5ad066aa26b8b28f20c06d2b8257f0c7e96fd
fringlesinthestreet <bicarcamo@uc.cl>
2018-09-11 12:25:45 -0300

    trying something..

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -14,3 +14,62 @@ def create_app():
     db.init_app(app)
 
     return app
+
+
+import os
+from flask import render_template, request, db, current_app as app
+from flaskr.models import Posts
+import datetime
+import time
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    if request.method == "POST":
+        try:
+            text = request.form['text']
+            ip = request.remote_addr
+            ts = time.time()
+            date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+            print(text, ip, date)
+            #posts.append(text)
+            post = Posts(text, ip, date)
+            db.session.add(post)
+            db.session.commit()
+        except KeyError:
+            errors.append(
+                "Bad Request (400)"
+            )
+        except Exception as err:
+            errors.append(
+                "Ocurrió un error: {}".format(err)
+            )
+
+    posts = Posts.query.order_by(Posts.id).all()
+
+    return render_template('index.html', errors=errors, posts=posts)
+
+
+@app.errorhandler(404)
+def url_error(e):
+    return """
+    Wrong URL!
+    <pre>{}</pre>""".format(e), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+@app.errorhandler(400)
+def bad_request(e):
+    return """
+    Bad Request attempted <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+if __name__ == '__main__':
+    app.run()
