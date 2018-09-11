f282e6dcde05f40656615e1f3ee4bef83859924c
fringlesinthestreet <bicarcamo@uc.cl>
2018-09-11 09:49:22 -0300

    created app factory

---
+++ flaskr/factory.py
@@ -0,0 +1,8 @@
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(os.environ['APP_SETTINGS'])
+
+    from models import db
+    db.init_app(app)
+
+    return app
--- flaskr/models.py
+++ flaskr/models.py
@@ -3,9 +3,6 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 
 
-
-app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
